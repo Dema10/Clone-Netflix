0969eb4ec7f067a88b910809451bfd2b55951b4a
“Dema10 <gabrieledematteis@me.com>
2024-04-08 03:37:17 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let ricerca = document.getElementById("ricerca");
+let lente = document.querySelector(".bi");
+
+lente.addEventListener("click", function () {
+    ricerca.classList.toggle("animazione");
+});
